e29152d039bc6eb9b645a0106a81e8eb231c6c98
Danilo Zivanovic <danzivanovi@etmlnet.local>
2023-02-27 15:58:41 +0100

    Modification

--- ProjetDemineur/Program.cs
+++ ProjetDemineur/Program.cs
@@ -8,6 +8,8 @@ namespace ProjetDemineur
     {
         static void Main(string[] args)
         {
+            int ligne = 0;
+            int colonne = 0;
             // Affcihe le titre
             Titre();
 
@@ -17,21 +19,24 @@ namespace ProjetDemineur
             // Demande a l'utilisateur les valeurs pour le tableau
             ConsigneTableau();
 
+            // Permet d'afficher le tableau du demineur
+            Tableau(ligne, colonne);
+        }
 
 
 
 
         /// <summary>
-            /// Affichage du titre principal
-            /// </summary>
+        /// Affichage du titre principal
+        /// </summary>
         static void Titre()
         {
-            
+
             Console.WriteLine("*****************************************************************************");
             Console.WriteLine("*                   Démineur simplifié (Danilo Zivanovic)                   *");
             Console.WriteLine("*****************************************************************************\n");
         }
-        
+
         /// <summary>
         /// Affichage des condition de nombre de lignes et colonnes
         /// </summary>
@@ -48,107 +53,188 @@ namespace ProjetDemineur
         /// Demande les valeurs a l'utilisateur
         /// </summary>
         static void ConsigneTableau()
+        {
+            int ligne = 0;
+            int colonne = 0;
+            int dificulte;
+            bool valueOk = true;
+
+
+            do
             {
-                int ligne = 0;
-                int colonne = 0;
-                int dificulte;
-                bool valueOk = true;
+                Console.Write("\nNombre de ligne : ");
 
-
-                do
+                if (!(int.TryParse(Console.ReadLine(), out ligne)))
                 {
-                    Console.Write("\nNombre de ligne : ");
-
-                    if (!(int.TryParse(Console.ReadLine(), out ligne)))
-                    {
-                        Console.WriteLine("Votre valeur n'est pas un nombre ! Merci de réessayer");
-                    }
-
-                    else if (ligne < 6 || ligne > 30)
-                    {
-                        Console.WriteLine("\nValeur hors limite ! Merci de réessayer !");
-                    }
-
-                    else if (ligne > 6 || ligne < 30)
-                    {
-                        break;
-                    }
-
-                } while (valueOk);
-
-                do
-                {
-                    Console.Write("\nNombre de colonne : ");
-
-                    if (!(int.TryParse(Console.ReadLine(), out colonne)))
-                    {
-                        Console.WriteLine("Votre valeur n'est pas un nombre ! Merci de réessayer");
-                    }
-
-                    else if (colonne < 6 || colonne > 30)
-                    {
-                        Console.WriteLine("\nValeur hors limite ! Merci de réessayer !");
-                    }
-
-                    else if (colonne > 6 || colonne < 30)
-                    {
-                        break;
-                    }
-
-                } while (valueOk);
-
-                Console.WriteLine("\n-----------------------------------------------------------------------------\n\tMerci d'entrer la difficulté pour votre jeu\n\ten sachant que :\n\t\t1 --> niveau facile\n\t\t2--> niveau moyen\n\t\t3 --> niveau difficile");
-                Console.Write("-----------------------------------------------------------------------------\n");
-
-                do
-                {
-                    Console.Write("\nvotre difficulté : ");
-
-                    if (!(int.TryParse(Console.ReadLine(), out dificulte)))
-                    {
-                        Console.WriteLine("Votre valeur n'est pas un nombre ! Merci de réessayer");
-                    }
-
-                    else if (dificulte <= 0 || dificulte >= 4)
-                    {
-                        Console.WriteLine("\nValeur hors limite ! Merci de réessayer !");
-                    }
-
-                    else if (dificulte >= 1 || dificulte <= 3)
-                    {
-                        break;
-                    }
-
-                } while (valueOk);
-
-                Console.WriteLine();
-                Tableau(ligne);
-                Console.ReadLine();
-
-            }
-
-            static void Tableau(int ligne)
-            {
-
-                for(int i = 0; i <= ligne-1; i++)
-                {
-                    //Début
-                    if (i == 0)
-                    {
-                        Console.Write("╔═══╦");
-                    }
-                    if (i > 0 && i < ligne-1)
-                    {
-                        Console.Write("═══╦");
-                    }
-                    if (i == ligne-1)
-                    {
-                        Console.Write("═══╗");
-                    }
+                    Console.WriteLine("Votre valeur n'est pas un nombre ! Merci de réessayer");
                 }
 
-            }
+                else if (ligne < 6 || ligne > 30)
+                {
+                    Console.WriteLine("\nValeur hors limite ! Merci de réessayer !");
+                }
+
+                else if (ligne > 6 || ligne < 30)
+                {
+                    break;
+                }
+
+            } while (valueOk);
+
+            do
+            {
+                Console.Write("\nNombre de colonne : ");
+
+                if (!(int.TryParse(Console.ReadLine(), out colonne)))
+                {
+                    Console.WriteLine("Votre valeur n'est pas un nombre ! Merci de réessayer");
+                }
+
+                else if (colonne < 6 || colonne > 30)
+                {
+                    Console.WriteLine("\nValeur hors limite ! Merci de réessayer !");
+                }
+
+                else if (colonne > 6 || colonne < 30)
+                {
+                    break;
+                }
+
+            } while (valueOk);
+
+            Console.WriteLine("\n-----------------------------------------------------------------------------\n\tMerci d'entrer la difficulté pour votre jeu\n\ten sachant que :\n\t\t1 --> niveau facile\n\t\t2--> niveau moyen\n\t\t3 --> niveau difficile");
+            Console.Write("-----------------------------------------------------------------------------\n");
+
+            do
+            {
+                Console.Write("\nvotre difficulté : ");
+
+                if (!(int.TryParse(Console.ReadLine(), out dificulte)))
+                {
+                    Console.WriteLine("Votre valeur n'est pas un nombre ! Merci de réessayer");
+                }
+
+                else if (dificulte <= 0 || dificulte >= 4)
+                {
+                    Console.WriteLine("\nValeur hors limite ! Merci de réessayer !");
+                }
+
+                else if (dificulte >= 1 || dificulte <= 3)
+                {
+                    break;
+                }
+
+            } while (valueOk);
+
+            Console.WriteLine();
+            TableauHaut(ligne, colonne);
+            Console.ReadLine();
 
         }
+
+        static void Tableau(int ligne, int colonne)
+        {
+            for (int i = 0; i <= ligne - 1; i++)
+            {
+                if (i == 0)
+                {
+                    for (int e = 0; e <= colonne - 1; e++)
+                    {
+
+                        if (e == 0)
+                        {
+                            Console.Write("╔═══╦");
+                        }
+                        if (e > 0 && e < colonne - 1)
+                        {
+                            Console.Write("═══╦");
+                        }
+                        if (e == colonne - 1)
+                        {
+                            Console.Write("═══╗");
+                            Console.WriteLine();
+
+                        }
+                    }
+                    for (int d = 0; d <= colonne - 1; d++)
+                    {
+                        if (d == 0)
+                        {
+                            Console.Write("║   ");
+                        }
+                        if (d > 0 && d < colonne - 1)
+                        {
+                            Console.Write("║   ");
+                        }
+                        if (d == colonne - 1)
+                        {
+                            Console.Write("║   ║");
+                            Console.WriteLine();
+
+                        }
+                    }
+                }
+                if (i == ligne - 1)
+                {
+                    for (int e = 0; e <= colonne - 1; e++)
+                    {
+                        if (e == 0)
+                        {
+                            Console.Write("╚═══");
+                        }
+                        if (e > 0 && e < colonne - 1)
+                        {
+                            Console.Write("╩═══");
+                        }
+                        if (e == colonne - 1)
+                        {
+                            Console.Write("╩═══╝");
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                else
+                {
+                    for (int e = 0; e <= colonne - 1; e++)
+                    {
+                        if (e == 0)
+                        {
+                            Console.Write("╠═══");
+                        }
+                        if (e > 0 && e < colonne - 1)
+                        {
+                            Console.Write("╬═══");
+                        }
+                        if (e == colonne - 1)
+                        {
+                            Console.Write("╬═══╣");
+                            Console.WriteLine();
+
+                        }
+                    }
+                    for (int d = 0; d <= colonne - 1; d++)
+                    {
+                        if (d == 0)
+                        {
+                            Console.Write("║   ");
+                        }
+                        if (d > 0 && d < colonne - 1)
+                        {
+                            Console.Write("║   ");
+                        }
+                        if (d == colonne - 1)
+                        {
+                            Console.Write("║   ║");
+                            Console.WriteLine();
+
+                        }
+                    }
+
+
+
+                }
+            }
+        }
+
     }
 }
